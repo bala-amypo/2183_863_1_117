78c43cad831fdf62091413841f2c423de00477fb
bala-amypo <bala@amypo.in>
2025-12-22 08:05:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private final String secret;
+    private final long validityInMs;
+    private final boolean isTestMode;
+
+    public JwtUtil(String secret, long validityInMs, boolean isTestMode) {
+        this.secret = secret;
+        this.validityInMs = validityInMs;
+        this.isTestMode = isTestMode;
+    }
+
+    public String generateToken(String subject, Long userId, String email, String role) {
+
+        Claims claims = Jwts.claims().setSubject(subject);
+        claims.put("userId", userId);
+        claims.put("email", email);
+        claims.put("role", role);
+
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityInMs);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getEmail(String token) {
+        return getClaims(token).get("email", String.class);
+    }
+
+    public String getRole(String token) {
+        return getClaims(token).get("role", String.class);
+    }
+
+    public Long getUserId(String token) {
+        return getClaims(token).get("userId", Long.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
