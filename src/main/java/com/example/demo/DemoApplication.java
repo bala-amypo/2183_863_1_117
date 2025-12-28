2bfa0c41cf53850089237b636b2ef184b6569b79
bala-amypo <bala@amypo.in>
2025-12-28 16:22:54 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,14 @@
-
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.example.demo.servlet")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplnjhb ication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
